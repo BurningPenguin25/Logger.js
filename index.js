e6f005d79db09b1225013f26ddeb4176e22bee7f
BurningPenguin25 <74684220+BurningPenguin25@users.noreply.github.com>
2022-07-30 10:49:59 +0300

    Логгер
     Добавление process.on

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-import {loggerConsole} from "./loggerConsole.js";
 import {loggerFile} from "./loggerFile.js";
 
-//loggerConsole({level: 'warn', message: 'Hello world'})
+loggerFile({level: 'warn', message: 'Hello world'} )
+loggerFile({level: 'warn', message: 'Hello world'} )
 loggerFile({level: 'warn', message: 'Hello world'} )
 
+
+process.on('SIGINT', loggerFile);
--- loggerFile.js
+++ loggerFile.js
@@ -2,47 +2,53 @@ import moment from 'moment' //  для формата даты время
 import {fs, mkdir, mkdirSync, writeFile} from "file-system"; // файловая система
 import config from './config.js'
 
-export const loggerFile = (options) => {
-    const levelName = getlevels(options.level) // указанный тип ошибки(error, debug, warn)
-    const message = options.message ?? 'Undentified Error' // если не указано сообщение, то в файл записывается 'Undentified Error'
 
+import express from "express"
+const exp = express()
 
+exp.get('/', (req, res) => {
+    res.send('Hello Sir')
+})
+exp.listen(3001, () => console.log('Server ready'))
 
-    writeToFile(levelName, message)
+export let loggerFile
+process.on('SIGBREAK', loggerFile = (options) => {
+             const levelName = getlevels(options.level) // указанный тип ошибки(error, debug, warn)
+            const message = options.message ?? 'Undentified Error' // если не указано сообщение, то в файл записывается 'Undentified Error'
+           writeToFile(levelName, message)
+})
 
-
-}
 
     let logsArray = []
-
     const  writeToFile = (levelName, message) => {
-
         const logsDir = './logs' // указание директория в котором моздается файл
-        let data = `{"level": "${levelName.toUpperCase()}", "message": "${message}", "timestamp": "${getFormatedCurrenDate()}" } \r\n`
-
+        let data = `{"level": "${levelName.toUpperCase()}", "message": "${message}", "timestamp": "${getFormatedCurrenDate()}" }\n`
         logsArray.push(data)
-        console.log(logsArray)
 
         if(!fs.existsSync(logsDir)){ //проверка наличия и создание папаки, если ее нет
             mkdirSync(logsDir)
+        } else if (logsArray.length == 3){
+            let stringLine = '';
+            logsArray.forEach(logsArr => (stringLine += logsArr))
+            console.log(stringLine)
+            //fs.appendFileSync(`./logs/${levelName}.log`, stringLine) // создание отдельного файла
+            fs.appendFileSync(`./logs/loggerFile.log`, stringLine)
+            logsArray = []
         }
-}
+    }
 
 
 
 
 
-// if(logsArray.length >= 500){
-//
-//     let stringLine = ''
-//     logs.forEach(log => (stringLine += JSON.stringify(stringLine)))
-//
-//     writeToFile(stringLine)
+
+
+
+// export const loggerFile = (options) => {
+//     const levelName = getlevels(options.level) // указанный тип ошибки(error, debug, warn)
+//     const message = options.message ?? 'Undentified Error' // если не указано сообщение, то в файл записывается 'Undentified Error'
+//     writeToFile(levelName, message)
 // }
-
-
-
-
 
 
 
@@ -60,5 +66,6 @@ const getlevels = (level) =>{
 const getFormatedCurrenDate = () => { // формат
     return moment(new Date()).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS)
 }
-//fs.appendFileSync(`./logs/${levelName}.log`, data) // создание отдельного файла
-// fs.appendFileSync(`./logs/logFile.log`, data) // запись логов в один файл
+
+
+
