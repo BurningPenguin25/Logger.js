e2f16d468d3652ff17d60767388c0b1d2e407aac
BurningPenguin25 <74684220+BurningPenguin25@users.noreply.github.com>
2022-08-02 11:06:23 +0300

    Логгер
     Добавление process

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import {loggerFile} from "./loggerFile.js";
 
-loggerFile({level: 'warn', message: 'Hello world'} )
-loggerFile({level: 'warn', message: 'Hello world'} )
-loggerFile({level: 'warn', message: 'Hello world'} )
 
 
--- loggerFile.js
+++ loggerFile.js
@@ -1,23 +1,73 @@
 import moment from 'moment' //  для формата даты время
 import {fs, mkdir, mkdirSync, writeFile} from "file-system"; // файловая система
-import config from './config.js'
+import process from "node:process"
 
+import express from 'express'
 
-import express from "express"
 const exp = express()
-
 exp.get('/', (req, res) => {
     res.send('Hello Sir')
 })
-exp.listen(3001, () => console.log('Server ready'))
+exp.listen(3002, () => console.log('Server ready'))
 
-export let loggerFile
-process.on('SIGBREAK', loggerFile = (options) => {
-             const levelName = getlevels(options.level) // указанный тип ошибки(error, debug, warn)
-            const message = options.message ?? 'Undentified Error' // если не указано сообщение, то в файл записывается 'Undentified Error'
-           writeToFile(levelName, message)
-})
 
+//error
+//info
+//warn
+//debug
+
+//access
+//system
+//databases
+//event
+//fatal
+
+
+    function loggerFileError() {
+    const levelName = "error" // указанный тип ошибки(error, debug, warn)
+    const message = "error message" // если не указано сообщение, то в файл записывается 'Undentified Error'
+    writeToFile(levelName, message)
+    }
+    process.on('SIGINT', loggerFileError);
+
+    function loggerFileInfo() {
+        const levelName = "error" // указанный тип ошибки(error, debug, warn)
+        const message = "error message" // если не указано сообщение, то в файл записывается 'Undentified Error'
+        writeToFile(levelName, message)
+    }
+    process.on('SIGINT', loggerFileInfo);
+
+    function loggerFileWarning() {
+        const levelName = "warning" // указанный тип ошибки(error, debug, warn)
+        const message = "warning message" // если не указано сообщение, то в файл записывается 'Undentified Error'
+        writeToFile(levelName, message)
+    }
+    process.on('warning', loggerFileWarning);
+
+    function loggerFileDebug() {
+        const levelName = "sigusr" // указанный тип ошибки(error, debug, warn)
+        const message = "sigusr message" // если не указано сообщение, то в файл записывается 'Undentified Error'
+        writeToFile(levelName, message)
+    }
+    process.on('SIGUSR1', loggerFileDebug);
+
+    function loggerFileSighup() {
+        const levelName = "sigusr" // указанный тип ошибки(error, debug, warn)
+        const message = "sigusr message" // если не указано сообщение, то в файл записывается 'Undentified Error'
+        writeToFile(levelName, message)
+    }
+    process.on('SIGHUP', loggerFileSighup);
+
+
+
+
+
+function loggerFileSigusr1() {
+    const levelName = "sigusr" // указанный тип ошибки(error, debug, warn)
+    const message = "sigusr message" // если не указано сообщение, то в файл записывается 'Undentified Error'
+    writeToFile(levelName, message)
+}
+process.on('SIGUSR1', loggerFileSigusr1);
 
     let logsArray = []
     const  writeToFile = (levelName, message) => {
@@ -27,7 +77,7 @@ process.on('SIGBREAK', loggerFile = (options) => {
 
         if(!fs.existsSync(logsDir)){ //проверка наличия и создание папаки, если ее нет
             mkdirSync(logsDir)
-        } else if (logsArray.length == 3){
+        } else if (logsArray.length >= 10){
             let stringLine = '';
             logsArray.forEach(logsArr => (stringLine += logsArr))
             console.log(stringLine)
@@ -40,28 +90,6 @@ process.on('SIGBREAK', loggerFile = (options) => {
 
 
 
-
-
-
-
-// export const loggerFile = (options) => {
-//     const levelName = getlevels(options.level) // указанный тип ошибки(error, debug, warn)
-//     const message = options.message ?? 'Undentified Error' // если не указано сообщение, то в файл записывается 'Undentified Error'
-//     writeToFile(levelName, message)
-// }
-
-
-
-
-
-
-
-
-
-
-const getlevels = (level) =>{
-    return level && config.levels.hasOwnProperty(level) ? level : 'info'
-}
 
 const getFormatedCurrenDate = () => { // формат
     return moment(new Date()).format(moment.HTML5_FMT.DATETIME_LOCAL_SECONDS)
